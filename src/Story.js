d07841cd251747863a8d666ce09672e4f356d304
Suman807 <81709848+Suman807@users.noreply.github.com>
2021-04-09 19:49:13 +0530

    Add files via upload

---
+++ src/Story.js
@@ -0,0 +1,14 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import "./Story.css";
+
+function Story({image,profilesrc,title}) {
+    return (
+        <div style={{backgroundImage: `url(${image})`}}  className="Story" >
+            <Avatar className="Story_avatar"src={profilesrc} />
+            <h4>{title}</h4>
+        </div>
+    )
+}
+
+export default Story;
